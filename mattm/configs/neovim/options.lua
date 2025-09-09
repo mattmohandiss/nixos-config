8ad79355faa4c68c93bf58e4bac30f8b9656d7c5
Matt Mohandiss <mattmohandiss@gmail.com>
2025-09-08 20:22:08 -0400

    Set neovim indentation to tabs

--- mattm/configs/neovim/options.lua
+++ mattm/configs/neovim/options.lua
@@ -4,3 +4,10 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.updatetime = 200
 vim.o.signcolumn = "yes"
+
+-- Tab settings - display tabs as 2 spaces wide but keep them as tab characters
+vim.o.tabstop = 2        -- Number of spaces that a tab character displays as
+vim.o.shiftwidth = 2     -- Number of spaces to use for each step of indentation
+vim.o.expandtab = false  -- Keep tabs as tab characters (don't convert to spaces)
+vim.o.smartindent = true -- Smart autoindenting for new lines
+vim.o.autoindent = true  -- Copy indent from current line when starting new line
