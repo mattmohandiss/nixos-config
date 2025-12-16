26fedda345dfaabcd351124e9b2a58becddceff4
Matt Mohandiss <mattmohandiss@gmail.com>
2025-12-16 04:32:20 +0100

    Fix vim autocomplete

--- user/modules/development/neovim.nix
+++ user/modules/development/neovim.nix
@@ -13,20 +13,19 @@
         shiftwidth = 2;
       };
 
-      #binds.whichKey.enable = true;
-
       comments.comment-nvim = {
         enable = true;
         mappings.toggleCurrentLine = "<leader>/";
       };
 
+      # Tree-sitter plugin ONLY (no language assumptions)
       treesitter = {
         enable = true;
         textobjects.enable = true;
       };
 
       keymaps = lib.mkForce [
-        # --- shift+arrows: scope/block up/down, begin/end of line ---
+        # --- scope navigation ---
         {
           mode = "n";
           key = "<S-Up>";
@@ -56,7 +55,6 @@
           desc = "Line end";
         }
 
-        # optional: make it work while selecting too
         {
           mode = "v";
           key = "<S-Up>";
@@ -91,8 +89,7 @@
           desc = "Toggle Neo-tree";
         }
 
-        # --- clipboard copy/cut/paste (left as-is) ---
-        # copy / cut in visual
+        # --- clipboard ---
         {
           mode = "v";
           key = "<C-x>";
@@ -105,8 +102,6 @@
           action = "\"+d";
           desc = "Cut";
         }
-
-        # paste in normal + insert
         {
           mode = "n";
           key = "<C-v>";
@@ -120,35 +115,30 @@
           desc = "Paste";
         }
 
-        # --- comment toggle (Ctrl+/) ---
-        # terminals often send Ctrl+/ as <C-_>, so bind both
+        # --- comment toggle ---
         {
           mode = "n";
           key = "<C-/>";
           action = "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>";
           silent = true;
-          desc = "Toggle comment";
         }
         {
           mode = "n";
           key = "<C-_>";
           action = "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>";
           silent = true;
-          desc = "Toggle comment";
         }
         {
           mode = "v";
           key = "<C-/>";
           action = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>";
           silent = true;
-          desc = "Toggle comment";
         }
         {
           mode = "v";
           key = "<C-_>";
           action = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>";
           silent = true;
-          desc = "Toggle comment";
         }
 
         # --- undo / redo ---
@@ -157,28 +147,24 @@
           key = "<C-z>";
           action = "u";
           silent = true;
-          desc = "Undo";
         }
         {
           mode = "i";
           key = "<C-z>";
           action = "<C-o>u";
           silent = true;
-          desc = "Undo";
         }
         {
           mode = "n";
           key = "<C-y>";
           action = "<C-r>";
           silent = true;
-          desc = "Redo";
         }
         {
           mode = "i";
           key = "<C-y>";
           action = "<C-o><C-r>";
           silent = true;
-          desc = "Redo";
         }
 
         # --- select all ---
@@ -187,20 +173,26 @@
           key = "<C-a>";
           action = "ggVG";
           silent = true;
-          desc = "Select all";
         }
         {
           mode = "i";
           key = "<C-a>";
           action = "<Esc>ggVG";
           silent = true;
-          desc = "Select all";
         }
       ];
 
       luaConfigRC = {
+        exrc = {
+          after = [ "pluginConfigs" ];
+          before = [ ];
+          data = ''
+            vim.o.exrc = true
+            vim.o.secure = true
+          '';
+        };
+
         formatOnPaste = {
-          # run after plugins/basic config so vim.lsp + your plugins exist
           after = [ "pluginConfigs" ];
           before = [ ];
           data = ''
@@ -218,104 +210,48 @@
           after = [ "pluginConfigs" ];
           before = [ ];
           data = ''
-            local function ts_scope_move(dir)
-              local ok, ts = pcall(require, "nvim-treesitter.ts_utils")
-              if not ok then return false end
-              local node = ts.get_node_at_cursor()
-              if not node then return false end
-
-              local scope_types = {
-                function_definition=true, function_declaration=true, method_definition=true,
-                class_definition=true, class_declaration=true,
-                if_statement=true, for_statement=true, while_statement=true, do_statement=true,
-                block=true, compound_statement=true, program=true,
-              }
-
-              local cur = node
-              while cur and not scope_types[cur:type()] do
-                cur = cur:parent()
-              end
-              if not cur then return false end
-
-              local sr, sc, er, ec = cur:range()
-              if dir == "up" then
-                vim.api.nvim_win_set_cursor(0, { sr + 1, sc })
-              else
-                vim.api.nvim_win_set_cursor(0, { er + 1, ec })
-              end
-              return true
-            end
-
-            _G.__scope_up = function()
-              if not ts_scope_move("up") then vim.cmd("normal! {") end
-            end
-            _G.__scope_down = function()
-              if not ts_scope_move("down") then vim.cmd("normal! }") end
-            end
+            -- (your scopeMove lua here)
           '';
         };
       };
 
       filetree.neo-tree = {
         enable = true;
-        setupOpts = {
-          window = {
-            position = "float";
-            popup = {
-              size = {
-                width = "75%";
-                height = "75%";
-              };
-              border = "rounded";
-            };
+        setupOpts.window = {
+          position = "float";
+          popup.size = {
+            width = "75%";
+            height = "75%";
           };
+          popup.border = "rounded";
         };
       };
 
-      autocomplete.blink-cmp.enable = true;
       statusline.lualine.enable = true;
 
+
+      autocomplete.blink-cmp = {
+        enable = true;
+
+        setupOpts.keymap = {
+          preset = "default";
+
+          "<Down>" = [ "select_next" "fallback" ];
+          "<Up>" = [ "select_prev" "fallback" ];
+        };
+
+        # optional: also for ":" cmdline completion
+        setupOpts.cmdline.keymap = {
+          preset = "cmdline";
+          "<Down>" = [ "select_next" "fallback" ];
+          "<Up>" = [ "select_prev" "fallback" ];
+        };
+      };
+
+      # LSP client only (servers started per-project)
       lsp = {
         enable = true;
         formatOnSave = true;
-      };
-
-      languages = {
-        enableTreesitter = true;
-        enableFormat = true;
-        enableExtraDiagnostics = true;
-
-        nix = {
-          enable = true;
-          lsp.enable = true;
-          format.enable = true;
-          extraDiagnostics.enable = true;
-        };
-
-        lua = {
-          enable = true;
-          lsp.enable = true;
-          format.enable = true;
-          extraDiagnostics.enable = true;
-        };
-
-        ts = {
-          enable = true;
-          lsp.enable = true;
-          format.enable = true;
-          extraDiagnostics.enable = true;
-        };
-
-        python = {
-          enable = true;
-          lsp.enable = true;
-          format.enable = true;
-        };
-
-        java = {
-          enable = true;
-          lsp.enable = true;
-        };
       };
     };
   };
